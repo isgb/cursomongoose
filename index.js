90df08f5b6756601e66cce8ddc9638a2965a52d7
oscar <oscar.vasquez@zentro.com.mx>
2024-02-19 16:02:37 -0600

    debug

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:2710/mywebstore');
+mongoose.connect('mongodb://127.0.0.1:2710/mywebstore',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
