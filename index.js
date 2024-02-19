9c682a4d948f332f7c4d765679c81cb626ce82d8
oscar <oscar.vasquez@zentro.com.mx>
2024-02-19 16:12:35 -0600

    debug

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:2710/mywebstore',{
+const uri = "mongodb://127.0.0.1:27017/mywebstore";
+const db = mongoose.connection;
+
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+})
+    .catch((error) => {
+        console.log(error);
+    })
 
+// https://www.youtube.com/watch?v=gfP3aqV38q4&t=14s
+// 17:43
+
+// escuchar eventos
+db.on('open', _ => {
+    console.log('Batabase is connected to',uri)
+})
+
+db.on('error', error => {
+    console.log(error)
+})
