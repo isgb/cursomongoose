fd52bfc24ec470cf2e0ef0d23bed139972530569
oscar <oscar.vasquez@zentro.com.mx>
2024-02-20 16:09:00 -0600

    debug

--- crud/findOne.js
+++ crud/findOne.js
@@ -2,8 +2,9 @@ require('../connection')
 
 const User = require('../models/User')
 
-function getUser() {
-
+async function getUser() {
+    const user = await User.findOne({username : 'fazt'});
+    console.log(user)
 }
 
 getUser();
